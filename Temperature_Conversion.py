e6b80d6dcc6e6d57c03fb0ba7ef2be4b29d62c40
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-01-10 20:45:48 -0800

    First attempt at Temperature Conversion

--- Temperature_Conversion.py
+++ Temperature_Conversion.py
@@ -1,1 +1,16 @@
+def to_celsius(fahrenheit):
+    celsius = (fahrenheit -32) * 5/9
+    return celsius
 
+def to_fahrenheit(celsius):
+    fahrenheit = celsius * 9/5 + 32
+    return fahrenheit
+
+
+if __name__ == "__main__":
+    for temp in range(0 ,212, 40):
+        print(temp, "Fahrenheit =", round(to_celsius(temp), 2), "Celsius")
+
+    for temp in range(0, 101, 20):
+        print(temp, "Celsius =", round(to_fahrenheit(temp), 2),
+              "Fahrenheit")
